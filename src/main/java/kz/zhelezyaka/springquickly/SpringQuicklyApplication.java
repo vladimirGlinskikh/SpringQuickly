51ba8d0c5986c1b21c9444623dd55843c271c85d
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-09-05 11:14:13 +0300

    Start project

---
+++ src/main/java/kz/zhelezyaka/springquickly/SpringQuicklyApplication.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.springquickly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuicklyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQuicklyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/zhelezyaka/springquickly/SpringQuicklyApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.springquickly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuicklyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
