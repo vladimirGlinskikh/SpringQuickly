457ff0e39a19413a06dc968f5f77fc57f7d66750
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-09-06 16:43:58 +0300

    Implementing values through class fields

---
+++ src/main/java/kz/zhelezyaka/springquickly/autowired/Person.java
@@ -0,0 +1,19 @@
+package kz.zhelezyaka.springquickly.autowired;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+    @Autowired
+    private Parrot parrot;
+
+    public String getName() {
+        return "Vladimir";
+    }
+
+    public Parrot getParrot() {
+        return parrot;
+    }
+}
