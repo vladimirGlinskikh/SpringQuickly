7d12a4e5ac7a7cfce9feab2e26a0e7d2198e340b
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-09-16 14:14:23 +0300

    Changing the parameters and return value of the intercepted method

--- src/main/java/kz/zhelezyaka/aspect/main/Main.java
+++ src/main/java/kz/zhelezyaka/aspect/main/Main.java
@@ -5,8 +5,13 @@ import kz.zhelezyaka.aspect.model.Comment;
 import kz.zhelezyaka.aspect.services.CommentService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.logging.Logger;
+
 public class Main {
+    private static Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
+
         var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
         var service = c.getBean(CommentService.class);
@@ -14,6 +19,8 @@ public class Main {
         Comment comment = new Comment();
         comment.setText("Test comment");
         comment.setAuthor("Vladimir");
-        service.publishComment(comment);
+
+        String value = service.publishComment(comment);
+        logger.info(value);
     }
 }
--- src/main/java/kz/zhelezyaka/aspect/services/CommentService.java
+++ src/main/java/kz/zhelezyaka/aspect/services/CommentService.java
@@ -9,12 +9,12 @@ import java.util.logging.Logger;
 public class CommentService {
     private Logger logger = Logger.getLogger(CommentService.class.getName());
 
-    public void publishComment(Comment comment) {
+    public String publishComment(Comment comment) {
         logger.info("Publishing comment: " + comment.getText());
+        return "SUCCESS";
     }
 
     public void setLogger(Logger logger) {
         this.logger = logger;
     }
-
 }
