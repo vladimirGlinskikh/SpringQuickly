ccf8af079dcefa427ea4330db4e07016e3fffc69
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-10-07 22:49:45 +0300

    Example Spring MVC

---
+++ src/main/java/kz/zhelezyaka/controller/MainController.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/home")
+    public String home() {
+        return "home.html";
+    }
+}
