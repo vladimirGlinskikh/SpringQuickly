d288ad2247b0b7806356d3a729849a5ec23e9611
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-10-08 15:52:20 +0300

    Transferring data from client to server via request parameters

--- src/main/java/kz/zhelezyaka/controller/MainController.java
+++ src/main/java/kz/zhelezyaka/controller/MainController.java
@@ -1,13 +1,19 @@
 package kz.zhelezyaka.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/home")
-    public String home() {
+    public String home(@RequestParam(required = false) String name,
+                       @RequestParam(required = false) String color,
+                       Model page) {
+        page.addAttribute("username", name);
+        page.addAttribute("color", color);
         return "home.html";
     }
 }
