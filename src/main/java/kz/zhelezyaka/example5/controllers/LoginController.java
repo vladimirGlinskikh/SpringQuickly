8a67243a4bd87f81f7348b6429a4a10e6613c801
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-10-08 20:23:51 +0300

    Using area beans visibilities within request

---
+++ src/main/java/kz/zhelezyaka/example5/controllers/LoginController.java
@@ -0,0 +1,38 @@
+package kz.zhelezyaka.example5.controllers;
+
+import kz.zhelezyaka.example5.model.LoginProcessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+    private final LoginProcessor loginProcessor;
+
+    public LoginController(LoginProcessor loginProcessor) {
+        this.loginProcessor = loginProcessor;
+    }
+
+    @GetMapping("/")
+    public String loginGet() {
+        return "login.html";
+    }
+
+    @PostMapping("/")
+    public String loginPost(@RequestParam String username,
+                            @RequestParam String password,
+                            Model model) {
+        loginProcessor.setUsername(username);
+        loginProcessor.setPassword(password);
+        boolean loggedIn = loginProcessor.login();
+
+        if (loggedIn) {
+            return "products.html";
+        } else {
+            model.addAttribute("message", "Login failed!");
+        }
+        return "login.html";
+    }
+}
---
+++ src/main/java/kz/zhelezyaka/example5/model/LoginProcessor.java
@@ -0,0 +1,38 @@
+package kz.zhelezyaka.example5.model;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Component
+@RequestScope
+public class LoginProcessor {
+    private String username;
+    private String password;
+
+    public boolean login() {
+        String username = this.getUsername();
+        String password = this.getPassword();
+
+        if ("Vladimir".equals(username) && "password".equals(password)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
