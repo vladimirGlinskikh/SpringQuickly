e791805587abcc4b1e95cfa9fdfe11029166e753
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-10-08 16:04:25 +0300

    Passing values from the client using path variables

--- src/main/java/kz/zhelezyaka/controller/MainController.java
+++ src/main/java/kz/zhelezyaka/controller/MainController.java
@@ -2,17 +2,16 @@ package kz.zhelezyaka.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @RequestMapping("/home")
-    public String home(@RequestParam(required = false) String name,
-                       @RequestParam(required = false) String color,
-                       Model page) {
-        page.addAttribute("username", name);
+    @RequestMapping("/home/{color}")
+    public String home(@PathVariable String color, Model page) {
+        page.addAttribute("username", "Vladimir");
         page.addAttribute("color", color);
         return "home.html";
     }
