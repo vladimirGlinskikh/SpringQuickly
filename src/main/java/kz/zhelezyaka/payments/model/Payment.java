fab065e7c81f842dd89a0f1acdd7cd38f950d7c6
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-10-12 19:30:41 +0300

    Calling a REST endpoint using OPENFEIGN

---
+++ src/main/java/kz/zhelezyaka/payments/model/Payment.java
@@ -0,0 +1,22 @@
+package kz.zhelezyaka.payments.model;
+
+public class Payment {
+    private String id;
+    private double amount;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
